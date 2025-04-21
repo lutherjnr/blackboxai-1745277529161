bd940a6d68d46f9c91ebcbb109905fa78902ad4e
Byrone Luther <103103850+lutherjnr@users.noreply.github.com>
2025-04-22 02:19:02 +0300

    Add frontend/src/api/axiosInstance.js 1745277541398

---
+++ frontend/src/api/axiosInstance.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { getToken, removeToken, removeUserRole } from '../utils/auth';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:8000/api/',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      removeToken();
+      removeUserRole();
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
